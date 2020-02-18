5fd23491776382689fd0d838b3e9278954d3842c
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-02-18 14:17:36 +0530

    Wheel ui

--- src/Components/Wheel.jsx
+++ src/Components/Wheel.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import "./Wheel.css";
-
+import next from "./next.svg";
+import play from "./play.svg";
+import prev from "./prev.svg";
 export default class Wheel extends Component {
   render() {
     return (
-      <div id="wheel-container">
-        <div id="center-btn"></div>
+      <div id="main-container">
+        <div id="wheel-container">
+          <div id="center-btn"></div>
+          <div id="menu">MENU</div>
+          <div id="next">
+            <img src={next} id="nextImg" alt="next" />
+          </div>
+          <div id="play">
+            <img src={play} id="playImg" alt="play" />
+          </div>
+          <div id="prev">
+            <img src={prev} id="prevImg" alt="prev" />
+          </div>
+        </div>
       </div>
     );
   }
