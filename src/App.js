b02967d3a6f215cb13feae9473004d1d8423e47b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-02-18 13:33:52 +0530

    Wheel css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Wheel from "./Components/Wheel";
+function App() {
+  return (
+    <div className="App">
+      <Wheel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Wheel.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Wheel.css";
+
+export default class Wheel extends Component {
+  render() {
+    return (
+      <div id="wheel-container">
+        <div id="center-btn"></div>
+      </div>
+    );
+  }
+}
