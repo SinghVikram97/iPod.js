f4cf27b7519e7e2975949ff2bc760ed64f146747
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-02-18 14:29:14 +0530

    zingtouch pan event

--- src/Components/Wheel.jsx
+++ src/Components/Wheel.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { createRef } from "react";
+import ZingTouch from "zingtouch";
 import "./Wheel.css";
 import next from "./next.svg";
 import play from "./play.svg";
 import prev from "./prev.svg";
+
 export default class Wheel extends Component {
+  nextBtn = createRef();
+  playBtn = createRef();
+  prevBtn = createRef();
+
+  componentDidMount() {
+    let nextRegion = ZingTouch.Region(this.nextBtn.current);
+    nextRegion.bind(this.nextBtn.current, "pan", e => {
+      console.log("next-btn");
+    });
+  }
+
   render() {
     return (
       <div id="main-container">
         <div id="wheel-container">
           <div id="center-btn"></div>
           <div id="menu">MENU</div>
-          <div id="next">
+          <div id="next" ref={this.nextBtn}>
             <img src={next} id="nextImg" alt="next" />
           </div>
           <div id="play">
